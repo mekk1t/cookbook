dd012760c747df402449c4a9c65ea3cc5899cafe
mekk1t <kit.kalimov@gmail.com>
2021-03-25 08:37:03 +0300

    Класс сервиса рецептов и модель рецепта

---
+++ Kernel/Models/Recipe.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+namespace KitProjects.MasterChef.Kernel.Models
+{
+    public class Recipe
+    {
+        public Guid Id { get; }
+        public string Title { get; }
+        public string Description { get; }
+    }
+}
---
+++ Kernel/Services/RecipeService.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KitProjects.MasterChef.Kernel.Services
+{
+    public class RecipeService
+    {
+        
+    }
+}
