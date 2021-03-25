c38e2f254346288da19d7f44f8f252cb30d46b0a
mekk1t <kit.kalimov@gmail.com>
2021-03-25 08:44:28 +0300

    Добавлена модель ингредиентов

---
+++ Kernel/Models/Ingredient.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections.Generic;
+
+namespace KitProjects.MasterChef.Kernel.Models
+{
+    public class Ingredient
+    {
+        public string Name { get; }
+        public ICollection<Category> Categories { get; }
+    }
+}
--- Kernel/Models/Recipe.cs
+++ Kernel/Models/Recipe.cs
@@ -1,8 +1,12 @@
-﻿namespace KitProjects.MasterChef.Kernel.Models
+﻿using System.Collections.Generic;
+
+namespace KitProjects.MasterChef.Kernel.Models
 {
     public class Recipe
     {
         public string Title { get; }
         public string Description { get; }
+        public ICollection<Category> Categories { get; set; }
+        public ICollection<Ingredient> Ingredients { get; set; }
     }
 }
