41fd9802dd9aa409561fd366ccddf8b60bd6490f
mekk1t <kit.kalimov@gmail.com>
2021-03-27 09:30:12 +0300

    Update Category.cs

--- Kernel/Models/Category.cs
+++ Kernel/Models/Category.cs
@@ -1,12 +1,22 @@
-﻿namespace KitProjects.MasterChef.Kernel.Models
-{
-    public class Category
-    {
-        public string Name { get; }
+﻿using System;
 
-        public Category(string name)
+namespace KitProjects.MasterChef.Kernel.Models
+{
+    public class Category : Entity
+    {
+        public Category()
+        {
+        }
+
+        public Category(Guid id) : base(id)
+        {
+        }
+
+        public Category(Guid id, string name) : base(id)
         {
             Name = name;
         }
+
+        public string Name { get; }
     }
 }
