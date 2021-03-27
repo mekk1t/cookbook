b3f7f7c733ad466b7f00320ed93c504c3162d743
mekk1t <kit.kalimov@gmail.com>
2021-03-27 10:23:50 +0300

    Проект тестов: ссылки

---
+++ Tests/Moderators/CategoryModeratorTests.cs
@@ -0,0 +1,16 @@
+﻿using KitProjects.MasterChef.Kernel;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KitProjects.MasterChef.Tests.Moderators
+{
+    public class CategoryModeratorTests
+    {
+        private readonly CategoryModerator sut;
+
+
+    }
+}
