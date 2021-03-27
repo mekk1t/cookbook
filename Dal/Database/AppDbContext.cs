426c1efd6deac91b8f58f01b4be42c74f0626fb6
mekk1t <kit.kalimov@gmail.com>
2021-03-27 10:23:04 +0300

    Update AppDbContext.cs

--- Dal/Database/AppDbContext.cs
+++ Dal/Database/AppDbContext.cs
@@ -1,13 +1,11 @@
-﻿using KitProjects.MasterChef.Dal.Database.Models;
+﻿using KitProjects.MasterChef.Kernel.Models;
 using Microsoft.EntityFrameworkCore;
 
 namespace KitProjects.MasterChef.Dal
 {
     public class AppDbContext : DbContext
     {
-        public DbSet<DbRecipe> Recipes { get; set; }
-        public DbSet<DbIngredient> Ingredients { get; set; }
-        public DbSet<DbCategory> Categories { get; set; }
+        public DbSet<Category> Categories { get; set; }
 
         public AppDbContext(DbContextOptions options) : base(options)
         {
