b5a34d5e894c36972150d5b7ec4312abea78f8cd
mekk1t <kit.kalimov@gmail.com>
2021-03-27 09:40:46 +0300

    Create CategoryController.cs

---
+++ WebApplication/Categories/CategoryController.cs
@@ -0,0 +1,18 @@
+﻿using KitProjects.MasterChef.Kernel.Abstractions;
+using KitProjects.MasterChef.Kernel.Models;
+using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
+
+namespace KitProjects.MasterChef.WebApplication.Categories
+{
+    [Route("categories")]
+    public class CategoryController : ApiControllerBase
+    {
+        [HttpGet("")]
+        public CategoryViewModel GetCategories([FromServices] IQuery<IEnumerable<Category>> getAllCategoriesQuery)
+        {
+            var categories = getAllCategoriesQuery.Execute();
+            return new CategoryViewModel(categories);
+        }
+    }
+}
