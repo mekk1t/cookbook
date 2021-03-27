2341584c075186efe6ca98bf4205ba0b11d54bef
mekk1t <kit.kalimov@gmail.com>
2021-03-27 09:41:03 +0300

    Create GetCategoriesResponse.cs

---
+++ WebApplication/Categories/GetCategoriesResponse.cs
@@ -0,0 +1,16 @@
+﻿using KitProjects.MasterChef.Kernel.Models;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace KitProjects.MasterChef.WebApplication.Categories
+{
+    public class GetCategoriesResponse
+    {
+        public IEnumerable<string> CategoryNames { get; }
+
+        public GetCategoriesResponse(IEnumerable<Category> categories)
+        {
+            this.CategoryNames = categories.Select(c => c.Name);
+        }
+    }
+}
