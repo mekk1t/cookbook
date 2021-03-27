fbc1d3031e08f0558e0ac0bf54f643939ebb6b67
mekk1t <kit.kalimov@gmail.com>
2021-03-27 09:40:41 +0300

    Create ApiControllerBase.cs

---
+++ WebApplication/ApiControllerBase.cs
@@ -0,0 +1,10 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace KitProjects.MasterChef.WebApplication
+{
+    [ApiController]
+    [Route("api")]
+    public class ApiControllerBase : ControllerBase
+    {
+    }
+}
