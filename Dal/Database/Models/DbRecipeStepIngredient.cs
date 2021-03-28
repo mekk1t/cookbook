36af70f7acdc1344c4e0d0e20b56eb0f28f88eda
mekk1t <kit.kalimov@gmail.com>
2021-03-28 21:20:25 +0300

    М:М Шаги рецепта и Ингредиенты

---
+++ Dal/Database/Models/DbRecipeStepIngredient.cs
@@ -0,0 +1,16 @@
+﻿using KitProjects.MasterChef.Kernel.Models.Ingredients;
+using System;
+
+namespace KitProjects.MasterChef.Dal.Database.Models
+{
+    public class DbRecipeStepIngredient
+    {
+        public Guid DbRecipeStepId { get; set; }
+        public Guid DbIngredientId { get; set; }
+        public DbRecipeStep Step { get; set; }
+        public DbIngredient Ingredient { get; set; }
+
+        public decimal Amount { get; set; }
+        public Measures Measure { get; set; }
+    }
+}
