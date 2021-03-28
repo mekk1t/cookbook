f66665c120f0bbbe0d4d14de3ba580a23f472e2c
mekk1t <kit.kalimov@gmail.com>
2021-03-28 21:20:14 +0300

    1:М Рецепты и шаги

---
+++ Dal/Database/Models/DbRecipe.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace KitProjects.MasterChef.Dal.Database.Models
+{
+    public class DbRecipe
+    {
+        public Guid Id { get; set; }
+        public string Title { get; set; }
+        public string Description { get; set; }
+        public ICollection<DbCategory> Categories { get; set; }
+        public IEnumerable<DbRecipeIngredient> RecipeIngredientLink { get; set; }
+        public ICollection<DbRecipeStep> Steps { get; set; }
+    }
+}
---
+++ Dal/Database/Models/DbRecipeStep.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace KitProjects.MasterChef.Dal.Database.Models
+{
+    public class DbRecipeStep
+    {
+        public Guid Id { get; set; }
+        public int Index { get; set; }
+        public string Description { get; set; }
+        public string Image { get; set; }
+        public IEnumerable<DbRecipeStepIngredient> StepIngredientsLink { get; set; }
+    }
+}
