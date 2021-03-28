693582f944a1d83a37b83b2278d77eaebb699b07
mekk1t <kit.kalimov@gmail.com>
2021-03-28 12:34:33 +0300

    Вью-модель категории

---
+++ WebApplication/Categories/CategoryViewModel.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace KitProjects.MasterChef.WebApplication.Categories
+{
+    public class CategoryViewModel
+    {
+        public Guid Id { get; }
+        public string Name { get; }
+        public IEnumerable<CategoryIngredientViewModel> Ingredients { get; }
+
+        public CategoryViewModel(Guid id, string name, IEnumerable<CategoryIngredientViewModel> ingredients)
+        {
+            Id = id;
+            Name = name;
+            Ingredients = ingredients;
+        }
+    }
+}
