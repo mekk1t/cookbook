7de5bc6ef8f12c878c902abbe92c786a95a1d065
mekk1t <kit.kalimov@gmail.com>
2021-03-28 12:34:22 +0300

    ViewModel для ингредиентов в рамках вью-модели категории

---
+++ WebApplication/Categories/CategoryIngredientViewModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace KitProjects.MasterChef.WebApplication.Categories
+{
+    public class CategoryIngredientViewModel
+    {
+        public Guid Id { get; }
+        public string Name { get; }
+
+        public CategoryIngredientViewModel(Guid id, string name)
+        {
+            Id = id;
+            Name = name;
+        }
+    }
+}
