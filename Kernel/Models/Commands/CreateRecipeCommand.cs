41489595c4db5988539df231557275443c80d603
mekk1t <kit.kalimov@gmail.com>
2021-03-28 20:42:04 +0300

    Модель команды на создание рецепта + бизнес-логика

---
+++ Kernel/Models/Commands/CreateRecipeCommand.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace KitProjects.MasterChef.Kernel.Models.Commands
+{
+    public class CreateRecipeCommand
+    {
+        public Guid Id { get; }
+        public string Title { get; }
+        public string Description { get; }
+        public IEnumerable<string> Categories { get; }
+        public IEnumerable<RecipeIngredientDetails> IngredientsDetails { get; }
+
+        public CreateRecipeCommand(Guid id, string title,
+            IEnumerable<string> categories,
+            IEnumerable<RecipeIngredientDetails> ingredientsDetails,
+            string description = "")
+        {
+            Id = id;
+            Title = title;
+            Description = description;
+            Categories = categories;
+            IngredientsDetails = ingredientsDetails;
+        }
+    }
+}
---
+++ Kernel/RecipeService.cs
@@ -0,0 +1,51 @@
+﻿using KitProjects.MasterChef.Kernel.Abstractions;
+using KitProjects.MasterChef.Kernel.Models.Commands;
+using KitProjects.MasterChef.Kernel.Models.Queries;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace KitProjects.MasterChef.Kernel
+{
+    public class RecipeService
+    {
+        private readonly ICommand<CreateRecipeCommand> _createRecipeCommand;
+        private readonly CategoryService _categoryService;
+        private readonly IngredientService _ingredientService;
+
+        public RecipeService(ICommand<CreateRecipeCommand> createRecipeCommand)
+        {
+            _createRecipeCommand = createRecipeCommand;
+        }
+
+        public void CreateRecipe(CreateRecipeCommand command)
+        {
+            if (command.IngredientsDetails?.Count() > 0)
+            {
+                foreach (var ingredient in command.IngredientsDetails)
+                {
+                    var oldIngredient = _ingredientService.GetIngredients(new GetIngredientsQuery(limit: 1000))
+                        .FirstOrDefault(i => i.Name == ingredient.IngredientName);
+                    if (oldIngredient == null)
+                    {
+                        _ingredientService.CreateIngredient(new CreateIngredientCommand(ingredient.IngredientName, new List<string>()));
+                    }
+                }
+            }
+
+            if (command.Categories?.Count() > 0)
+            {
+                foreach (var category in command.Categories)
+                {
+                    var oldCategory = _categoryService.GetCategories(new GetCategoriesQuery(limit: 1000))
+                        .FirstOrDefault(c => c.Name == category);
+                    if (oldCategory == null)
+                    {
+                        _categoryService.CreateCategory(new CreateCategoryCommand(category));
+                    }
+                }
+            }
+
+            _createRecipeCommand.Execute(command);
+        }
+    }
+}
