99c476863ff0188098428f819882bee0d4f77bdc
mekk1t <kit.kalimov@gmail.com>
2021-04-10 23:29:17 +0300

    Перемещение моделей

---
+++ Kernel/Recipes/Commands/Ingredients/AppendRecipeIngredientCommand.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace KitProjects.MasterChef.Kernel.Recipes.Commands.Ingredients
+{
+    public class AppendRecipeIngredientCommand
+    {
+        public Guid RecipeId { get; }
+        public Guid IngredientId { get; }
+
+        public AppendRecipeIngredientCommand(Guid recipeId, Guid ingredientId)
+        {
+            RecipeId = recipeId;
+            IngredientId = ingredientId;
+        }
+    }
+}
