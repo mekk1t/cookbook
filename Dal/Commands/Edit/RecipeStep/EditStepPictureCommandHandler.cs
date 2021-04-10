22c1ba75d6591d60c04ff6a7eae7d89aa22bf768
mekk1t <kit.kalimov@gmail.com>
2021-04-10 11:55:59 +0300

    реализация редактирования картинки шага рецепта

---
+++ Dal/Commands/Edit/RecipeStep/EditStepPictureCommandHandler.cs
@@ -0,0 +1,30 @@
+﻿using KitProjects.MasterChef.Kernel.Abstractions;
+using KitProjects.MasterChef.Kernel.Recipes.Commands;
+using System;
+using System.Linq;
+
+namespace KitProjects.MasterChef.Dal.Commands.Edit.RecipeStep
+{
+    public class EditStepPictureCommandHandler : ICommand<EditStepPictureCommand>
+    {
+        private readonly AppDbContext _dbContext;
+
+        public EditStepPictureCommandHandler(AppDbContext dbContext)
+        {
+            _dbContext = dbContext;
+        }
+
+        public void Execute(EditStepPictureCommand command)
+        {
+            var step = _dbContext.Recipes
+                .Where(r => r.Steps.Select(s => s.Id).Contains(command.StepId))
+                .Select(r => r.Steps.FirstOrDefault(step => step.Id == command.StepId))
+                ?.FirstOrDefault();
+            if (step == null)
+                throw new ArgumentException(null, nameof(command));
+
+            step.Image = command.NewImage;
+            _dbContext.SaveChanges();
+        }
+    }
+}
