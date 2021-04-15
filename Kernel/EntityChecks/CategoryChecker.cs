8109b5db03c1c9f60cb430c3bdad5ed5eacd2abf
mekk1t <kit.kalimov@gmail.com>
2021-04-15 23:43:32 +0300

    Реализация проверки категории на существование

---
+++ Kernel/EntityChecks/CategoryChecker.cs
@@ -0,0 +1,29 @@
+﻿using KitProjects.MasterChef.Kernel.Abstractions;
+using KitProjects.MasterChef.Kernel.Extensions;
+using KitProjects.MasterChef.Kernel.Models;
+using KitProjects.MasterChef.Kernel.Models.Commands;
+
+namespace KitProjects.MasterChef.Kernel.EntityChecks
+{
+    public class CategoryChecker : IEntityChecker<Category, string>
+    {
+        private readonly IQuery<Category, SearchCategoryQuery> _searchCategory;
+
+        public CategoryChecker(IQuery<Category, SearchCategoryQuery> searchCategory)
+        {
+            _searchCategory = searchCategory;
+        }
+
+        public bool CheckExistence(string parameters = null)
+        {
+            if (parameters.IsNullOrEmpty())
+                return false;
+
+            var category = _searchCategory.Execute(new SearchCategoryQuery(parameters));
+            if (category == null)
+                return false;
+
+            return true;
+        }
+    }
+}
