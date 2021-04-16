4aaf84c311e12da7b80aecd01f11b216dfe410de
mekk1t <kit.kalimov@gmail.com>
2021-04-16 22:24:07 +0300

    Update AppendCategoryToIngredientDecorator.cs

--- Kernel/Decorators/AppendCategoryToIngredientDecorator.cs
+++ Kernel/Decorators/AppendCategoryToIngredientDecorator.cs
@@ -1,37 +1,39 @@
 ﻿using KitProjects.MasterChef.Kernel.Abstractions;
 using KitProjects.MasterChef.Kernel.Ingredients.Commands;
 using KitProjects.MasterChef.Kernel.Models;
-using KitProjects.MasterChef.Kernel.Models.Commands;
-using KitProjects.MasterChef.Kernel.Models.Queries;
+using KitProjects.MasterChef.Kernel.Models.Queries.Get;
 using System;
+using System.Linq;
 
 namespace KitProjects.MasterChef.Kernel.Decorators
 {
     public class AppendCategoryToIngredientDecorator : ICommand<AppendIngredientCategoryCommand>
     {
-        private readonly IQuery<Category, SearchCategoryQuery> _searchCategory;
-        private readonly IQuery<Ingredient, SearchIngredientQuery> _searchIngredient;
+        private readonly IEntityChecker<Category, string> _categoryChecker;
+        private readonly IQuery<Ingredient, GetIngredientQuery> _getIngredient;
         private readonly ICommand<AppendIngredientCategoryCommand> _decoratee;
 
         public AppendCategoryToIngredientDecorator(
-            IQuery<Category, SearchCategoryQuery> searchCategory,
-            IQuery<Ingredient, SearchIngredientQuery> searchIngredient,
+            IEntityChecker<Category, string> categoryChecker,
+            IQuery<Ingredient, GetIngredientQuery> getIngredient,
             ICommand<AppendIngredientCategoryCommand> decoratee)
         {
-            _searchCategory = searchCategory;
-            _searchIngredient = searchIngredient;
+            _categoryChecker = categoryChecker;
+            _getIngredient = getIngredient;
             _decoratee = decoratee;
         }
 
         public void Execute(AppendIngredientCategoryCommand command)
         {
-            var appendCategory = _searchCategory.Execute(new SearchCategoryQuery(command.CategoryName));
-            if (appendCategory == null)
+            bool categoryExists = _categoryChecker.CheckExistence(command.CategoryName);
+            if (!categoryExists)
                 throw new InvalidOperationException();
-            var existingIngredient = _searchIngredient.Execute(new SearchIngredientQuery(command.IngredientId));
+
+            var existingIngredient = _getIngredient.Execute(new GetIngredientQuery(command.IngredientId));
             if (existingIngredient == null)
                 throw new ArgumentException(null, nameof(command));
-            if (existingIngredient.Categories.Contains(appendCategory))
+
+            if (existingIngredient.Categories.Select(c => c.Name).Contains(command.CategoryName))
                 throw new ArgumentException("Нельзя добавить одну и ту же категорию дважды.");
 
             _decoratee.Execute(command);
