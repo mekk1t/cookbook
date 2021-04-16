e447c12bcc4164ef888000e5a2c9117e81badb13
mekk1t <kit.kalimov@gmail.com>
2021-04-17 00:01:20 +0300

    Вынос замены списка ингредиентов

---
+++ Kernel/Decorators/ReplaceIngredientsListInRecipeDecorator.cs
@@ -0,0 +1,49 @@
+﻿using KitProjects.MasterChef.Kernel.Abstractions;
+using KitProjects.MasterChef.Kernel.Models;
+using KitProjects.MasterChef.Kernel.Models.Commands;
+using KitProjects.MasterChef.Kernel.Recipes.Commands.Ingredients;
+using System;
+using System.Linq;
+
+namespace KitProjects.MasterChef.Kernel.Decorators
+{
+    public class ReplaceIngredientsListInRecipeDecorator : ICommand<ReplaceIngredientsListCommand>
+    {
+        private readonly ICommand<ReplaceIngredientsListCommand> _decoratee;
+        private readonly IEntityChecker<Ingredient, string> _ingredientChecker;
+        private readonly IEntityChecker<Recipe, Guid> _recipeChecker;
+        private readonly ICommand<CreateIngredientCommand> _createIngredient;
+
+        public ReplaceIngredientsListInRecipeDecorator(
+            ICommand<ReplaceIngredientsListCommand> decoratee,
+            IEntityChecker<Ingredient, string> ingredientChecker,
+            IEntityChecker<Recipe, Guid> recipeChecker,
+            ICommand<CreateIngredientCommand> createIngredient)
+        {
+            _decoratee = decoratee;
+            _ingredientChecker = ingredientChecker;
+            _recipeChecker = recipeChecker;
+            _createIngredient = createIngredient;
+        }
+
+        public void Execute(ReplaceIngredientsListCommand command)
+        {
+            bool recipeExists = _recipeChecker.CheckExistence(command.RecipeId);
+            if (!recipeExists)
+                throw new ArgumentException(null, nameof(command));
+
+            foreach (var ingredient in command.NewIngredients)
+            {
+                bool ingredientExists = _ingredientChecker.CheckExistence(ingredient.Name);
+                if (!ingredientExists)
+                {
+                    _createIngredient.Execute(new CreateIngredientCommand(
+                        ingredient.Name,
+                        ingredient.Categories.Select(c => c.Name)));
+                }
+            }
+
+            _decoratee.Execute(command);
+        }
+    }
+}
--- Kernel/Editors/RecipeIngredientEditor.cs
+++ Kernel/Editors/RecipeIngredientEditor.cs
@@ -10,44 +10,12 @@ namespace KitProjects.MasterChef.Kernel.Recipes
 {
     public class RecipeIngredientEditor
     {
-        private readonly IQuery<Ingredient, SearchIngredientQuery> _searchIngredient;
-        private readonly IQuery<Recipe, SearchRecipeQuery> _searchRecipe;
-        private readonly ICommand<ReplaceIngredientsListCommand> _replaceIngredientsList;
-        private readonly ICommand<CreateIngredientCommand> _createIngredient;
         private readonly ICommand<EditRecipeIngredientDescriptionCommand> _editIngredientDescription;
 
         public RecipeIngredientEditor(
-            IQuery<Ingredient, SearchIngredientQuery> searchIngredient,
-            IQuery<Recipe, SearchRecipeQuery> searchRecipe,
-            ICommand<ReplaceIngredientsListCommand> replaceIngredientsList,
-            ICommand<CreateIngredientCommand> createIngredient,
             ICommand<EditRecipeIngredientDescriptionCommand> editIngredientDescription)
         {
-            _searchIngredient = searchIngredient;
-            _searchRecipe = searchRecipe;
-            _replaceIngredientsList = replaceIngredientsList;
-            _createIngredient = createIngredient;
             _editIngredientDescription = editIngredientDescription;
-        }
-
-        public void ReplaceIngredientsList(Ingredient[] newIngredients, Guid recipeId)
-        {
-            var recipe = _searchRecipe.Execute(new SearchRecipeQuery(recipeId));
-            if (recipe == null)
-                throw new ArgumentException(null, nameof(recipeId));
-
-            foreach (var ingredient in newIngredients)
-            {
-                var existingIngredient = _searchIngredient.Execute(new SearchIngredientQuery(ingredient.Name));
-                if (existingIngredient == null)
-                {
-                    _createIngredient.Execute(new CreateIngredientCommand(
-                        ingredient.Name,
-                        ingredient.Categories.Select(c => c.Name)));
-                }
-            }
-
-            _replaceIngredientsList.Execute(new ReplaceIngredientsListCommand(newIngredients, recipeId));
         }
 
         public void EditIngredientsDescription(EditRecipeIngredientDescriptionCommand command)
