1a135d2ce9a13502fb37b083bd874ea37c258cfb
mekk1t <kit.kalimov@gmail.com>
2021-04-17 11:42:48 +0300

    Модель для полной замены ингредиентов в шаге

---
+++ Kernel/Models/Steps/ReplaceStepIngredientsCommand.cs
@@ -0,0 +1,14 @@
+﻿namespace KitProjects.MasterChef.Kernel.Models.Steps
+{
+    public class ReplaceStepIngredientsCommand
+    {
+        public RecipeStepIds Ids { get; }
+        public Ingredient[] Ingredients { get; }
+
+        public ReplaceStepIngredientsCommand(RecipeStepIds ids, Ingredient[] ingredients)
+        {
+            Ids = ids;
+            Ingredients = ingredients;
+        }
+    }
+}
