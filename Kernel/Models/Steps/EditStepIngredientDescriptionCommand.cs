e453ce42317d1f580deed492bc8dbbf93b5b869b
mekk1t <kit.kalimov@gmail.com>
2021-04-17 11:31:34 +0300

    Модель редактирования данных ингредиента в шаге

---
+++ Kernel/Models/Steps/EditStepIngredientDescriptionCommand.cs
@@ -0,0 +1,17 @@
+﻿using KitProjects.MasterChef.Kernel.Models.Ingredients;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KitProjects.MasterChef.Kernel.Models.Steps
+{
+    public class EditStepIngredientDescriptionCommand
+    {
+        public Guid RecipeId { get; }
+        public Guid StepId { get; }
+        public decimal Amount { get; }
+        public Measures Measure { get; }
+    }
+}
