971dd43c94aa30bd531b78b2abb97350690d56a6
mekk1t <kit.kalimov@gmail.com>
2021-04-17 11:25:04 +0300

    Модель команды на удаление ингредиента из шага

---
+++ Kernel/Models/Steps/RemoveIngredientFromStepCommand.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace KitProjects.MasterChef.Kernel.Models.Steps
+{
+    public class RemoveIngredientFromStepCommand
+    {
+        public Guid RecipeId { get; }
+        public Guid StepId { get; }
+        public Guid IngredientId { get; }
+
+        public RemoveIngredientFromStepCommand(Guid recipeId, Guid stepId, Guid ingredientId)
+        {
+            RecipeId = recipeId;
+            StepId = stepId;
+            IngredientId = ingredientId;
+        }
+    }
+}
