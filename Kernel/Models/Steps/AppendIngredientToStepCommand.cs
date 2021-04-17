eaeb4c0b74863eac98240560a35fcea9c3903178
mekk1t <kit.kalimov@gmail.com>
2021-04-17 11:26:53 +0300

    Модель команды на добавление ингредиента в шаг

---
+++ Kernel/Models/Steps/AppendIngredientToStepCommand.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace KitProjects.MasterChef.Kernel.Models.Steps
+{
+    public class AppendIngredientToStepCommand
+    {
+        public Guid RecipeId { get; }
+        public Guid StepId { get; }
+        public Ingredient Ingredient { get; }
+
+        public AppendIngredientToStepCommand(Guid recipeId, Guid stepId, Ingredient ingredient)
+        {
+            RecipeId = recipeId;
+            StepId = stepId;
+            Ingredient = ingredient;
+        }
+    }
+}
