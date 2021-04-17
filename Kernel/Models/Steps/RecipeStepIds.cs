262be1bbd5188d137bd3d93b45349dfdb7297a85
mekk1t <kit.kalimov@gmail.com>
2021-04-17 11:39:33 +0300

    Модель группировки ID рецепта и шага + модель для замены ингредиента в шаге

---
+++ Kernel/Models/Steps/RecipeStepIds.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace KitProjects.MasterChef.Kernel.Models.Steps
+{
+    public class RecipeStepIds
+    {
+        public Guid Recipe { get; }
+        public Guid Step { get; }
+
+        public RecipeStepIds(Guid recipeId, Guid stepId)
+        {
+            Recipe = recipeId;
+            Step = stepId;
+        }
+    }
+}
