f767e7011ada39b19142e4cfcfef6beaa08437bc
mekk1t <kit.kalimov@gmail.com>
2021-04-17 11:02:27 +0300

    Update DependencyInjectionExtensions.cs

--- WebApplication/Extensions/DependencyInjectionExtensions.cs
+++ WebApplication/Extensions/DependencyInjectionExtensions.cs
@@ -6,6 +6,7 @@ using KitProjects.MasterChef.Kernel.Ingredients.Commands;
 using KitProjects.MasterChef.Kernel.Models.Commands;
 using KitProjects.MasterChef.Kernel.Recipes;
 using KitProjects.MasterChef.Kernel.Recipes.Commands;
+using KitProjects.MasterChef.Kernel.Recipes.Commands.Ingredients;
 using SimpleInjector;
 using System;
 
@@ -17,12 +18,28 @@ namespace KitProjects.MasterChef.WebApplication.Extensions
         {
             var assemblies = AppDomain.CurrentDomain.GetAssemblies();
             container.Register(typeof(ICommand<>), assemblies);
+
             container.RegisterDecorator<ICommand<CreateCategoryCommand>, CreateCategoryDecorator>();
             container.RegisterDecorator<ICommand<CreateIngredientCommand>, CreateIngredientDecorator>();
+            container.RegisterDecorator<ICommand<CreateRecipeCommand>, CreateRecipeDecorator>();
+
             container.RegisterDecorator<ICommand<AppendIngredientCategoryCommand>, AppendCategoryToIngredientDecorator>();
             container.RegisterDecorator<ICommand<RemoveIngredientCategoryCommand>, RemoveCategoryFromIngredientDecorator>();
+            container.RegisterDecorator<ICommand<ReplaceRecipeIngredientCommand>, ReplaceIngredientInRecipeDecorator>();
+            container.RegisterDecorator<ICommand<ReplaceIngredientsListCommand>, ReplaceIngredientsListInRecipeDecorator>();
+            container.RegisterDecorator<ICommand<SwapStepsCommand>, SwapRecipeStepsDecorator>();
+
             container.RegisterDecorator<ICommand<AppendCategoryToRecipeCommand>, AppendCategoryToRecipeDecorator>();
             container.RegisterDecorator<ICommand<RemoveRecipeCategoryCommand>, RemoveCategoryFromRecipeDecorator>();
+
+            container.RegisterDecorator<ICommand<AppendRecipeIngredientCommand>, AppendIngredientToRecipeDecorator>();
+            container.RegisterDecorator<ICommand<EditRecipeIngredientDescriptionCommand>, EditRecipeIngredientDescriptionDecorator>();
+            container.RegisterDecorator<ICommand<RemoveRecipeIngredientCommand>, RemoveIngredientFromRecipeDecorator>();
+
+            container.RegisterDecorator<ICommand<AppendRecipeStepCommand>, AppendStepDecorator>();
+            container.RegisterDecorator<ICommand<EditStepDescriptionCommand>, EditRecipeStepDescriptionDecorator>();
+            container.RegisterDecorator<ICommand<EditStepPictureCommand>, EditRecipeStepPictureDecorator>();
+            container.RegisterDecorator<ICommand<RemoveRecipeStepCommand>, RemoveStepFromRecipeDecorator>();
 
             container.Register(typeof(IQuery<,>), assemblies);
             container.Register(typeof(IQuery<>), assemblies);
