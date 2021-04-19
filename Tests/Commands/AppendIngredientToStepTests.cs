50c2242be31f53869309b19a712b10872892798c
mekk1t <kit.kalimov@gmail.com>
2021-04-19 20:32:55 +0300

    Первые тесты команды добавления ингредиента в шаг

---
+++ Tests/Commands/AppendIngredientToStepTests.cs
@@ -0,0 +1,64 @@
+﻿using FluentAssertions;
+using KitProjects.Fixtures;
+using KitProjects.MasterChef.Dal;
+using KitProjects.MasterChef.Dal.Commands;
+using KitProjects.MasterChef.Dal.Commands.Edit.Recipe;
+using KitProjects.MasterChef.Dal.Queries.Categories;
+using KitProjects.MasterChef.Dal.Queries.Ingredients;
+using KitProjects.MasterChef.Dal.Queries.Recipes;
+using KitProjects.MasterChef.Kernel;
+using KitProjects.MasterChef.Kernel.Abstractions;
+using KitProjects.MasterChef.Kernel.Decorators;
+using KitProjects.MasterChef.Kernel.EntityChecks;
+using KitProjects.MasterChef.Kernel.Models;
+using KitProjects.MasterChef.Kernel.Models.Steps;
+using System;
+using Xunit;
+
+namespace KitProjects.MasterChef.Tests.Commands
+{
+    [Collection("Db")]
+    public sealed class AppendIngredientToStepTests : IDisposable
+    {
+        private readonly AppDbContext _dbContext;
+        private readonly DbFixture _fixture;
+        private readonly ICommand<AppendIngredientToStepCommand> _sut;
+
+        public AppendIngredientToStepTests(DbFixture fixture)
+        {
+            _fixture = fixture;
+            _dbContext = _fixture.DbContext;
+            var getRecipeQueryHandler = new GetRecipeQueryHandler(_dbContext);
+            var ingredientChecker = new IngredientChecker(new GetIngredientQueryHandler(_dbContext));
+            _sut =
+                new AppendIngredientToStepDecorator(
+                    null,
+                    getRecipeQueryHandler,
+                    new AppendIngredientToRecipeDecorator(
+                        new AppendIngredientCommandHandler(_dbContext),
+                        new RecipeChecker(getRecipeQueryHandler),
+                        ingredientChecker,
+                        new CreateIngredientDecorator(
+                            new CreateIngredientCommandHandler(_dbContext),
+                            ingredientChecker,
+                            new CreateCategoryDecorator(
+                                new CreateCategoryCommandHandler(_dbContext),
+                                new CategoryChecker(
+                                    new GetCategoryQueryHandler(_dbContext))))));
+        }
+
+        [Fact]
+        public void Cant_append_step_to_nonexistent_recipe()
+        {
+            Action act = () => _sut.Execute(
+                new AppendIngredientToStepCommand(
+                    new RecipeStepIds(Guid.NewGuid(), Guid.NewGuid()),
+                    null,
+                    null));
+
+            act.Should().ThrowExactly<EntityNotFoundException>();
+        }
+
+        public void Dispose() => _dbContext.Dispose();
+    }
+}
