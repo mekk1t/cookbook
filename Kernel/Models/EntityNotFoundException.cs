b56c31b0e0c4420b45c2691f507b2d5089e76f5e
mekk1t <kit.kalimov@gmail.com>
2021-04-19 20:16:20 +0300

    Перемещение модели исключения EntityNotFound

---
+++ Kernel/Models/EntityNotFoundException.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace KitProjects.MasterChef.Kernel.Models
+{
+    public class EntityNotFoundException : Exception
+    {
+        public override string Message { get; }
+
+        public EntityNotFoundException()
+        {
+        }
+
+        public EntityNotFoundException(string message) : base(message)
+        {
+        }
+
+        public EntityNotFoundException(string message, Exception innerException) : base(message, innerException)
+        {
+        }
+
+        public EntityNotFoundException(Type type)
+        {
+            Message = $"Сущность типа {type} не была найдена.";
+        }
+
+        public EntityNotFoundException(Guid id)
+        {
+            Message = $"Сущность с ID {id} не была найдена.";
+        }
+
+        public EntityNotFoundException(Type type, Guid id)
+        {
+            Message = $"Сущность типа {type} c ID {id} не была найдена.";
+        }
+    }
+}
