03a7abeafd9b8b21b72b2301a0ab171cbd3e54c5
mekk1t <kit.kalimov@gmail.com>
2021-04-25 23:49:24 +0300

    Миграция страниц в веб-проект

---
+++ StaticSite/Controllers/AdminController.cs
@@ -0,0 +1,9 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace StaticSite.Controllers
+{
+    public class AdminController : Controller
+    {
+        public IActionResult Index() => View();
+    }
+}
