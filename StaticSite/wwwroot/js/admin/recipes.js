3afb22cd956664e9d38b63c916cbc89556dfac4a
mekk1t <kit.kalimov@gmail.com>
2021-05-02 13:01:26 +0300

    Инициализация простейшей формы

--- StaticSite/wwwroot/js/admin/recipes.js
+++ StaticSite/wwwroot/js/admin/recipes.js
@@ -1,4 +1,9 @@
-﻿window.onload = function () {
+﻿const _pageBody = document.getElementById("admin-body");
+let _previousPageState = _pageBody.innerHTML;
+
+initAddRecipeEvent();
+
+window.onload = function () {
     fetch('https://localhost:5001/recipes')
         .then(response => {
             return response.json();
@@ -32,10 +37,49 @@
         });
 }
 
+function renderAddRecipeForm() {
+    _previousPageState = _pageBody.innerHTML;
+    clearPage();
+    let _addForm = getAddRecipeForm();
+    _pageBody.appendChild(_addForm);
+}
+
+function clearPage() {
+    _pageBody.innerHTML = '';
+}
+
 function td(innerHtml, id = null) {
     let result = document.createElement("td");
     result.innerHTML = innerHtml;
     if (id != null)
         result.id = id;
     return result;
+}
+
+function initAddRecipeEvent() {
+    let _add = document.querySelector(".add");
+    _add.addEventListener("click", renderAddRecipeForm);
+}
+
+function getAddRecipeForm() {
+    let _form = document.createElement("form");
+    _form.classList.add("add-recipe-form");
+    let _titleInput = document.createElement("input");
+    _titleInput.name = "recipe-title";
+    let _submitButton = document.createElement("button");
+    _submitButton.type = "submit";
+    _submitButton.textContent = "Создать";
+    let _backButton = document.createElement("button");
+    _backButton.type = "button";
+    _backButton.textContent = "Назад";
+    _backButton.addEventListener("click", function () {
+        _pageBody.innerHTML = _previousPageState;
+        initAddRecipeEvent();
+    })
+
+    _form.appendChild(_titleInput);
+    _form.appendChild(_submitButton);
+    _form.appendChild(_backButton);
+
+    return _form;
 }
