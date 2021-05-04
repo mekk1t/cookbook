a9cd590b805cd8f4eb70e3628799dc612c8fd7ac
mekk1t <kit.kalimov@gmail.com>
2021-05-04 11:08:18 +0300

    верстка страницы с ингредиентами

---
+++ StaticSite/wwwroot/js/admin/ingredients.js
@@ -0,0 +1,21 @@
+﻿const _pageBody = document.getElementById("admin-body");
+let _previousPageState = _pageBody.innerHTML;
+boldLink("#admin-ingredients-link");
+
+window.onload = function () {
+    fetch('https://localhost:5001/ingredients')
+        .then(response => {
+            return response.json();
+        })
+        .then(data => {
+            let _ingredientsList = selectFirst("ul.ingredients-list");
+            for (let ingredient of data.ingredients) {
+                let _li = _new("li");
+                _li.textContent = ingredient.name;
+                _li.id = ingredient.id;
+                _ingredientsList.appendChild(_li);
+            }
+
+            appendActionsToList("ul.ingredients-list");
+        });
+}
