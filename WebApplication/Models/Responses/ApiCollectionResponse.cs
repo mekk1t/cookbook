eca1eb0928b4d3358630f0a7fd760e19ad9e479c
mekk1t <kit.kalimov@gmail.com>
2021-06-09 23:27:07 +0300

    Создал модели для одиночного и коллекционных ответов апи

---
+++ WebApplication/Models/Responses/ApiCollectionResponse.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace KitProjects.MasterChef.WebApplication.Models.Responses
+{
+    /// <summary>
+    /// Модель ответа от сервера с коллекцией данных.
+    /// </summary>
+    /// <typeparam name="T">Тип данных в коллекции.</typeparam>
+    public class ApiCollectionResponse<T>
+    {
+        /// <summary>
+        /// Отступ для получения данных далее по списку с текущими параметрами.
+        /// </summary>
+        public int? NextOffset { get; }
+        /// <summary>
+        /// Есть ли по текущим параметрам ещё данные.
+        /// </summary>
+        public bool? HasMoreItems { get; }
+        /// <summary>
+        /// Коллекция данных.
+        /// </summary>
+        public IEnumerable<T> Items { get; }
+
+        public ApiCollectionResponse(IEnumerable<T> items) => Items = items ?? Array.Empty<T>();
+
+        public ApiCollectionResponse(IEnumerable<T> items, int nextOffset, bool hasMoreItems)
+        {
+            Items = items ?? Array.Empty<T>();
+            NextOffset = nextOffset;
+            HasMoreItems = hasMoreItems;
+        }
+    }
+}
---
+++ WebApplication/Models/Responses/ApiResponse.cs
@@ -0,0 +1,14 @@
+﻿namespace KitProjects.MasterChef.WebApplication.Models.Responses
+{
+    /// <summary>
+    /// Модель ответа от сервера с данными об объекте.
+    /// </summary>
+    /// <typeparam name="T">Тип данных в ответе сервера.</typeparam>
+    public class ApiResponse<T>
+    {
+        /// <summary>
+        /// Ответ от сервера.
+        /// </summary>
+        public T Data { get; }
+    }
+}
