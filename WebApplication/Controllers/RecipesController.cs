592430284a603e9a55088b9526f826e7a4b127f0
mekk1t <kit.kalimov@gmail.com>
2021-06-19 00:09:59 +0300

    Update RecipesController.cs

--- WebApplication/Controllers/RecipesController.cs
+++ WebApplication/Controllers/RecipesController.cs
@@ -4,9 +4,9 @@ using KitProjects.MasterChef.Kernel.Recipes;
 using KitProjects.MasterChef.WebApplication.ApplicationServices;
 using KitProjects.MasterChef.WebApplication.Controllers;
 using KitProjects.MasterChef.WebApplication.Models.Filters;
+using KitProjects.MasterChef.WebApplication.Models.Responses;
 using Microsoft.AspNetCore.Mvc;
 using System;
-using System.Collections.Generic;
 using System.Linq;
 
 namespace KitProjects.MasterChef.WebApplication.Recipes
@@ -21,38 +21,61 @@ namespace KitProjects.MasterChef.WebApplication.Recipes
         }
 
         /// <summary>
+        /// Получает список рецептов.
+        /// </summary>
+        [HttpGet]
+        [ProducesResponseType(typeof(ApiCollectionResponse<Recipe>), 200)]
+        [ProducesResponseType(typeof(ApiErrorResponse), 500)]
+        public IActionResult GetRecipes([FromQuery] PaginationFilter filter) =>
+            ProcessRequest(() =>
+            {
+                return new ApiCollectionResponse<Recipe>(_crud.Read(filter));
+            });
+
+        /// <summary>
         /// Создает рецепт.
         /// </summary>
         /// <param name="request">Запрос на создание рецепта.</param>
         [HttpPost]
-        public IActionResult CreateRecipe([FromBody] CreateRecipeRequest request)
-        {
-            _crud.Create(
-                new CreateRecipeCommand(
-                    Guid.NewGuid(),
-                    request.Title,
-                    request.Categories,
-                    request.Ingredients.Select(details => new RecipeIngredientDetails(details.IngredientName, details.Measure, details.Amount, details.Notes)),
-                    request.Steps.Select(step => new RecipeStep
-                    {
-                        Description = step.Description,
-                        Image = step.Image
-                    })));
-            return Ok();
-        }
-
-        /// <summary>
-        /// Получает список рецептов. Включает все связи.
-        /// </summary>
-        [HttpGet]
-        public IEnumerable<Recipe> GetRecipes(PaginationFilter filter) => _crud.Read(filter);
+        [ProducesResponseType(200)]
+        [ProducesResponseType(typeof(ApiErrorResponse), 500)]
+        public IActionResult CreateRecipe([FromBody] CreateRecipeRequest request) =>
+            ProcessRequest(() =>
+            {
+                _crud.Create(
+                    new CreateRecipeCommand(
+                        Guid.NewGuid(),
+                        request.Title,
+                        request.Categories,
+                        request.Ingredients
+                            .Select(details =>
+                                new RecipeIngredientDetails(
+                                    details.IngredientName,
+                                    details.Measure,
+                                    details.Amount,
+                                    details.Notes)),
+                        request.Steps
+                            .Select(step =>
+                                new RecipeStep
+                                {
+                                    Description = step.Description,
+                                    Image = step.Image
+                                })));
+                return Ok();
+            });
 
         /// <summary>
         /// Получает подробную информацию о рецепте.
         /// </summary>
         /// <param name="recipeId">ID рецепта.</param>
         [HttpGet("{recipeId}")]
-        public RecipeDetails GetRecipe([FromRoute] Guid recipeId) => _crud.Read(recipeId);
+        [ProducesResponseType(typeof(ApiResponse<RecipeDetails>), 200)]
+        [ProducesResponseType(typeof(ApiErrorResponse), 500)]
+        public IActionResult GetRecipe([FromRoute] Guid recipeId) =>
+            ProcessRequest(() =>
+            {
+                return new ApiResponse<RecipeDetails>(_crud.Read(recipeId));
+            });
 
         /// <summary>
         /// Редактирует название и описание рецепта по ID.
@@ -60,23 +83,19 @@ namespace KitProjects.MasterChef.WebApplication.Recipes
         /// <param name="recipeId">ID рецепта в формате GUID.</param>
         /// <param name="request">Запрос на редактирование рецепта.</param>
         [HttpPut("{recipeId}")]
-        public IActionResult EditRecipe(
-            [FromRoute] Guid recipeId,
-            [FromBody] EditRecipeRequest request)
-        {
-            _crud.Update(recipeId, request.NewTitle, request.NewDescription);
-            return Ok();
-        }
+        [ProducesResponseType(200)]
+        [ProducesResponseType(typeof(ApiErrorResponse), 500)]
+        public IActionResult EditRecipe([FromRoute] Guid recipeId, [FromBody] EditRecipeRequest request) =>
+            ProcessRequest(() => _crud.Update(recipeId, request.NewTitle, request.NewDescription));
 
         /// <summary>
         /// Удаляет рецепт по ID.
         /// </summary>
         /// <param name="recipeId">ID в формате GUID.</param>
         [HttpDelete("{recipeId}")]
-        public IActionResult DeleteRecipe([FromRoute] Guid recipeId)
-        {
-            _crud.Delete(recipeId);
-            return Ok();
-        }
+        [ProducesResponseType(200)]
+        [ProducesResponseType(typeof(ApiErrorResponse), 500)]
+        public IActionResult DeleteRecipe([FromRoute] Guid recipeId) =>
+            ProcessRequest(() => _crud.Delete(recipeId));
     }
 }
