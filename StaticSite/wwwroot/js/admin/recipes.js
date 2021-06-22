fc8c783999c64cc0aa47b506792d775b47ffb3fa
mekk1t <kit.kalimov@gmail.com>
2021-06-22 12:47:47 +0300

    Рефакторинг скриптов

--- StaticSite/wwwroot/js/admin/recipes.js
+++ StaticSite/wwwroot/js/admin/recipes.js
@@ -1,80 +1,25 @@
-﻿const _pageBody = document.getElementById("admin-body");
-let _previousPageState = _pageBody.innerHTML;
-boldLink("#admin-recipes-link");
-
-initAddRecipeEvent();
+﻿boldLink("#admin-recipes-link");
 
 window.onload = function () {
-    fetch('https://localhost:5001/recipes')
-        .then(response => {
-            return response.json();
-        })
-        .then(data => {
-            let _recipesList = document.querySelector("ul.recipes-list");
-            for (let recipe of data) {
-                let _li = _new("li");
-                _li.textContent = recipe.title;
-                _li.id = recipe.id;
-                _li.classList.add("recipe");
-                _recipesList.appendChild(_li);
-            }
+    _fetch("recipes")
+        .then(json => {
+            let recipes = json.items;
+            let table = document.querySelector('table tbody');
+            for (let recipe of recipes) {
+                let tableRow = document.createElement('tr');
+                let name = document.createElement('td');
+                name.innerHTML = recipe.title;
+                let id = document.createElement('td');
+                id.classList.add('w3-hide');
+                id.innerHTML = recipe.id;
+                let description = document.createElement('td');
+                description.innerHTML = recipe.description;
 
-            appendActionsToList("ul.recipes-list");
+                tableRow.appendChild(id);
+                tableRow.appendChild(name);
+                tableRow.appendChild(description);
+
+                table.appendChild(tableRow);
+            }
         });
-}
-
-function renderAddRecipeForm() {
-    _previousPageState = _pageBody.innerHTML;
-    clearPage();
-    let _addForm = getAddRecipeForm();
-    _pageBody.appendChild(_addForm);
-}
-
-function clearPage() {
-    _pageBody.innerHTML = '';
-}
-
-function initAddRecipeEvent() {
-    let _add = document.querySelector(".add-recipe");
-    _add.addEventListener("click", renderAddRecipeForm);
-}
-
-function getAddRecipeForm() {
-    let _form = document.createElement("form");
-    _form.classList.add("add-recipe-form");
-    let _titleInput = document.createElement("input");
-    _titleInput.name = "recipe-title";
-    let _submitButton = document.createElement("button");
-    _submitButton.type = "submit";
-    _submitButton.textContent = "Создать";
-    let _backButton = document.createElement("button");
-    _backButton.type = "button";
-    _backButton.textContent = "Назад";
-    _backButton.addEventListener("click", function () {
-        _pageBody.innerHTML = _previousPageState;
-        initAddRecipeEvent();
-    })
-
-    _form.appendChild(_titleInput);
-    _form.appendChild(_submitButton);
-    _form.appendChild(_backButton);
-
-    _form.addEventListener("submit", function (event) {
-        event.preventDefault();
-        let requestBody = {
-            categories: [],
-            ingredients: [],
-            steps: [],
-            title: _titleInput.value
-        };
-        fetch('https://localhost:5001/recipes', {
-            method: 'POST',
-            body: JSON.stringify(requestBody),
-            headers: {
-                'Content-Type': 'application/json;charset=utf-8',
-            }
-        })
-    });
-
-    return _form;
 }
--- StaticSite/wwwroot/js/requests.js
+++ StaticSite/wwwroot/js/requests.js
@@ -1,7 +1,11 @@
-﻿function getCategories() {
-    let categories = fetch('https://localhost:5001/categories')
-        .then(response => {
-            return response.json();
-        });
-    return categories;
+﻿const BASE_URL = "https://localhost:5001"
+
+/**
+ * Отправляет запрос в апи по эндпоинту. Возвращает промис с ответом сервера в формате JSON.
+ * @param {string} endpoint
+ * @returns {Promise}
+ */
+function _fetch(endpoint) {
+    let promise = fetch(`${BASE_URL}/api/${endpoint}`).then(response => response.json());
+    return promise;
 }
