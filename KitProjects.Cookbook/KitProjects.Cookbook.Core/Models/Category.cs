07c103939978cb9e889b22c534c651ac35695469
mekk1t <kit.kalimov@gmail.com>
2021-07-21 00:23:14 +0300

    Добавлен тип категории в модель категории.

--- KitProjects.Cookbook/KitProjects.Cookbook.Core/Models/Category.cs
+++ KitProjects.Cookbook/KitProjects.Cookbook.Core/Models/Category.cs
@@ -3,6 +3,7 @@
     public class Category : Entity
     {
         public string Name { get; set; }
+        public CategoryType Type { get; set; }
 
         public Category()
         {
---
+++ KitProjects.Cookbook/KitProjects.Cookbook.Core/Models/CategoryType.cs
@@ -0,0 +1,9 @@
+﻿namespace KitProjects.Cookbook.Core.Models
+{
+    public enum CategoryType
+    {
+        Default = 0,
+        Recipe,
+        Ingredient
+    }
+}
