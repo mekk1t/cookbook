37a4bd78cd8a55d311374d9af684f77ffe04e53b
mekk1t <kit.kalimov@gmail.com>
2021-08-06 23:33:43 +0300

    Абстрактная реализация репозитория.

---
+++ KitProjects.Cookbook.UI/KitProjects.Cookbook.Database/Repository.cs
@@ -0,0 +1,41 @@
+﻿using KitProjects.Cookbook.Domain.Models;
+using Microsoft.EntityFrameworkCore;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace KitProjects.Cookbook.Database
+{
+    public abstract class Repository<T> where T : Entity
+    {
+        private readonly CookbookDbContext _dbContext;
+
+        public Repository(CookbookDbContext dbContext)
+        {
+            _dbContext = dbContext;
+        }
+
+        public void Delete(T entity)
+        {
+            _dbContext.Set<T>().Remove(entity);
+            _dbContext.SaveChanges();
+        }
+
+        public void Save(T entity)
+        {
+            _dbContext.Set<T>().Update(entity);
+            _dbContext.SaveChanges();
+        }
+
+        public abstract T GetDetails(long id);
+
+        public List<T> GetList(long lastId = 0, int limit = 10)
+        {
+            return _dbContext.Set<T>()
+                .AsNoTracking()
+                .Where(r => r.Id > lastId)
+                .OrderBy(e => e.Id)
+                .Take(limit)
+                .ToList();
+        }
+    }
+}
