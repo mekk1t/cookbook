1ec77baf395f10b74e91c0d4ece03a9b553aeb1e
mekk1t <kit.kalimov@gmail.com>
2021-08-07 22:27:41 +0300

    Update New.cshtml.cs

--- KitProjects.Cookbook.UI/KitProjects.Cookbook.UI/Pages/Admin/Recipes/New.cshtml.cs
+++ KitProjects.Cookbook.UI/KitProjects.Cookbook.UI/Pages/Admin/Recipes/New.cshtml.cs
@@ -1,8 +1,6 @@
 ﻿using KitProjects.Cookbook.Database;
 using KitProjects.Cookbook.Domain.Models;
-using Microsoft.AspNetCore.Mvc.ModelBinding;
 using Microsoft.AspNetCore.Mvc.RazorPages;
-using System.Collections.Generic;
 
 namespace KitProjects.Cookbook.UI.Pages.Admin.Recipes
 {
@@ -15,40 +13,10 @@ namespace KitProjects.Cookbook.UI.Pages.Admin.Recipes
             _repository = repository;
         }
 
-        [BindRequired]
-        public string Title { get; set; }
-        [BindRequired]
-        public string Description { get; set; }
-        [BindRequired]
-        public List<string> Tags { get; set; }
-        [BindRequired]
-        public List<Category> Categories { get; set; }
-        [BindRequired]
-        public List<IngredientDetails> Ingredients { get; set; }
-        [BindRequired]
-        public List<Step> Steps { get; set; }
-        [BindRequired]
-        public int CookingDuration { get; set; }
-        [BindRequired]
-        public List<CookingType> CookingTypes { get; set; }
-        [BindRequired]
-        public Difficulty Difficulty { get; set; }
-
         public void OnGet() { }
-        public void OnPost()
+        public void OnPost(Recipe recipe)
         {
-            _repository.Save(new Recipe
-            {
-                Title = Title,
-                Description = Description,
-                Tags = Tags,
-                Categories = Categories,
-                Difficulty = Difficulty,
-                IngredientDetails = Ingredients,
-                CookingTypes = CookingTypes,
-                Steps = Steps,
-                CookingDuration = CookingDuration
-            });
+            _repository.Save(recipe);
         }
     }
 }
