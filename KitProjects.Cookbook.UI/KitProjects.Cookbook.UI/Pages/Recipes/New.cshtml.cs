2515300fd8f7c2ea56ada1180d5884b678b1ca66
mekk1t <kit.kalimov@gmail.com>
2021-08-16 18:09:38 +0300

    Написан хэндлер страницы нового рецепта для обработки нового ингредиента

--- KitProjects.Cookbook.UI/KitProjects.Cookbook.UI/Pages/Recipes/New.cshtml.cs
+++ KitProjects.Cookbook.UI/KitProjects.Cookbook.UI/Pages/Recipes/New.cshtml.cs
@@ -2,23 +2,29 @@
 using KitProjects.Cookbook.Domain.Models;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
-using System;
 
 namespace KitProjects.Cookbook.UI.Pages.Recipes
 {
     public class NewModel : PageModel
     {
+        private readonly Repository<Ingredient> _ingredientRepository;
         private readonly RecipeRepository _repository;
         [BindProperty] public Recipe Recipe { get; set; }
 
-        public NewModel(RecipeRepository repository)
+        public NewModel(RecipeRepository repository, Repository<Ingredient> ingredientRepository)
         {
+            _ingredientRepository = ingredientRepository;
             _repository = repository;
         }
 
         public void OnPost()
         {
             _repository.Save(Recipe);
+        }
+
+        public void OnPostNewIngredient([FromBody] Ingredient ingredient)
+        {
+            _ingredientRepository.Save(ingredient);
         }
     }
 }
