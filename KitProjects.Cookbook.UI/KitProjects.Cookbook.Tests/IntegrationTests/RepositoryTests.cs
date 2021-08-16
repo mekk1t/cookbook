8b8c1c65195373981460e38e39cebec25bcc7568
mekk1t <kit.kalimov@gmail.com>
2021-08-16 17:30:21 +0300

    Добавлен проект тестов и протестировано взаимодействие с БД

---
+++ KitProjects.Cookbook.UI/KitProjects.Cookbook.Tests/IntegrationTests/RepositoryTests.cs
@@ -0,0 +1,135 @@
+﻿using FluentAssertions;
+using KitProjects.Cookbook.Database;
+using KitProjects.Cookbook.Domain.Models;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using Xunit;
+
+namespace KitProjects.Cookbook.Tests.IntegrationTests
+{
+    public class RepositoryTests : IDisposable
+    {
+        private readonly CookbookDbContext _dbContext;
+        private readonly Repository<Recipe> _sut;
+
+        public RepositoryTests()
+        {
+            _dbContext =
+                new CookbookDbContext(
+                    new DbContextOptionsBuilder<CookbookDbContext>()
+                        .UseSqlServer("Server=localhost;Database=Cookbook_TESTS;Trusted_Connection=True;").Options);
+            _dbContext.Database.EnsureCreated();
+            _sut = new Repository<Recipe>(_dbContext);
+        }
+
+        [Fact]
+        public void Можно_сохранить_рецепт_без_связанных_сущностей()
+        {
+            var recipe = new Recipe
+            {
+                Description = "Описание",
+                Synopsis = "Синопсис",
+                Difficulty = Difficulty.Hard,
+                CookingTypes = new List<CookingType> { CookingType.Frying },
+                Title = "Название",
+                Tags = new List<string> { "Тег1", "Тег2" },
+                CookingDuration = 12,
+                Categories = new List<Category> { Category.Breakfast, Category.Lunch, Category.Healthy }
+            };
+
+            _sut.Save(recipe);
+
+            recipe.Id.Should().NotBe(default);
+        }
+
+        [Fact]
+        public void Можно_сохранить_ингредиент()
+        {
+            var ingredient = new Ingredient
+            {
+                Name = "Огурец",
+                Type = IngredientType.Vegetables
+            };
+            var sut = new Repository<Ingredient>(_dbContext);
+
+            sut.Save(ingredient);
+
+            ingredient.Id.Should().NotBe(default);
+        }
+
+        [Fact]
+        public void Можно_сохранить_рецепт_с_ингредиентами()
+        {
+            var ingredient = new Ingredient { Name = "Ингредиент" };
+            new Repository<Ingredient>(_dbContext).Save(ingredient);
+            _dbContext.ChangeTracker.Clear();
+            var recipe = new Recipe
+            {
+                IngredientDetails = new List<IngredientDetails>
+                {
+                    new IngredientDetails
+                    {
+                        Ingredient = ingredient,
+                        Amount = 12,
+                        Measure = IngredientMeasure.gm,
+                        Optional = false
+                    }
+                }
+            };
+
+            _sut.Save(recipe);
+
+            recipe.Id.Should().NotBe(default);
+            recipe.IngredientDetails[0].Ingredient.Id.Should().Be(ingredient.Id);
+        }
+
+        [Fact]
+        public void Можно_сохранить_рецепт_с_шагами_с_ингредиентами()
+        {
+            var ingredient = new Ingredient { Name = "Ингредиент" };
+            new Repository<Ingredient>(_dbContext).Save(ingredient);
+            _dbContext.ChangeTracker.Clear();
+            var recipe = new Recipe
+            {
+                IngredientDetails = new List<IngredientDetails>
+                {
+                    new IngredientDetails
+                    {
+                        Ingredient = ingredient,
+                        Amount = 12,
+                        Measure = IngredientMeasure.gm,
+                        Optional = false
+                    }
+                },
+                Steps = new List<Step>
+                {
+                    new Step
+                    {
+                        Description = "Описание",
+                        Order = 1,
+                        IngredientDetails = new List<IngredientDetails>
+                        {
+                            new IngredientDetails
+                            {
+                                Ingredient = ingredient,
+                                Amount = 1,
+                                Measure = IngredientMeasure.gm,
+                                Optional = true
+                            }
+                        }
+                    }
+                }
+            };
+
+            _sut.Save(recipe);
+
+            recipe.Id.Should().NotBe(default);
+            ingredient.Id.Should().NotBe(default);
+            recipe.IngredientDetails[0].Ingredient.Id.Should().Be(ingredient.Id);
+            recipe.Steps[0].IngredientDetails[0].Ingredient.Id.Should().Be(ingredient.Id);
+        }
+
+        public void Dispose() => _dbContext.Dispose();
+    }
+}
