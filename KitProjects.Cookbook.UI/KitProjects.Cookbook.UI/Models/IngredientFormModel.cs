ec30ec3ce01ecca4cc12575a64bebd4762190a9b
mekk1t <kit.kalimov@gmail.com>
2021-08-17 12:11:09 +0300

    Создана модель для рендера компонента ингредиента в форме

---
+++ KitProjects.Cookbook.UI/KitProjects.Cookbook.UI/Models/IngredientFormModel.cs
@@ -0,0 +1,19 @@
+﻿using KitProjects.Cookbook.Domain.Models;
+
+namespace KitProjects.Cookbook.UI.Models
+{
+    public class IngredientFormModel
+    {
+        /// <summary>
+        /// Приставка к названию инпутов ингредиента. Является названием свойств, содержащих поля объекта <see cref="IngredientDetails"/>.
+        /// </summary>
+        /// <remarks>
+        /// Например, <c>Recipe.Steps[0]</c> или <c>Recipe</c>.
+        /// </remarks>
+        public string Prefix { get; set; }
+        /// <summary>
+        /// Номер ингредиента в содержащем списке ингредиентов.
+        /// </summary>
+        public int Order { get; set; }
+    }
+}
