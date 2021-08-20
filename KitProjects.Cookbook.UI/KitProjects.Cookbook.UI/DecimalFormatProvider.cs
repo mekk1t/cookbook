06a33dc88453f0994790d385e171cc37e390be64
mekk1t <kit.kalimov@gmail.com>
2021-08-20 22:40:11 +0300

    Реализовал переопределение форматирования Decimal-значений под свои нужды.

---
+++ KitProjects.Cookbook.UI/KitProjects.Cookbook.UI/DecimalFormatProvider.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Linq;
+
+namespace KitProjects.Cookbook.UI
+{
+    public class DecimalFormatProvider : IFormatProvider, ICustomFormatter
+    {
+        public string Format(string format, object arg, IFormatProvider formatProvider)
+        {
+            string numericString = arg.ToString();
+
+            var splits = numericString.Split('.');
+            if (splits[1].All(_char => _char == '0'))
+                return splits[0];
+
+            return numericString;
+        }
+
+        public object GetFormat(Type formatType)
+        {
+            if (formatType == typeof(ICustomFormatter))
+                return this;
+
+            return null;
+        }
+    }
+}
---
+++ KitProjects.Cookbook.UI/KitProjects.Cookbook.UI/Extensions.cs
@@ -0,0 +1,10 @@
+﻿namespace KitProjects.Cookbook.UI
+{
+    public static class Extensions
+    {
+        private readonly static DecimalFormatProvider _formatProvider = new();
+
+        public static string ToCustomString(this decimal value) =>
+            string.Format(_formatProvider, "{0}", value);
+    }
+}
