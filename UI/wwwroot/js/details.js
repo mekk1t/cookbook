b5a136915febd62b07baaef17ef22f73350b7187
mekk1t <kit.kalimov@gmail.com>
2021-10-17 11:32:54 +0300

    Реализация удаления рецепта

---
+++ UI/wwwroot/js/details.js
@@ -0,0 +1,8 @@
+﻿function deleteRecipe(recipeId) {
+    $.ajax({
+        url: `${window.location.origin}/api/recipes/${recipeId}`,
+        method: 'DELETE'
+    }).done(function (result) {
+        window.location.replace(window.location.origin + "/recipes");
+    });
+}
