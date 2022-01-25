f388fc793f51b3c53d49bfc784f9819739ef3b57
mekk1t <kit.kalimov@gmail.com>
2022-01-25 11:50:06 +0300

    Рефакторю модель пользователя

--- src/KP.Cookbook.Domain/Entities/User.cs
+++ src/KP.Cookbook.Domain/Entities/User.cs
@@ -1,17 +1,45 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace KP.Cookbook.Domain.Entities
+﻿namespace KP.Cookbook.Domain.Entities
 {
     public class User : Entity
     {
-        public string Nickname { get; set; }
-        public string Avatar { get; set; }
-        public DateTime JoinedAt { get; set; }
-        public string Login { get; set; }
-        public string PasswordHash { get; set; }
+        public string? Nickname { get; set; }
+        public string? Avatar { get; set; }
+
+        public UserType UserType { get; }
+        public DateTime JoinedAt { get; }
+        public string Login { get; }
+        public string PasswordHash { get; }
+
+        public User(UserType userType)
+        {
+            UserType = userType;
+            JoinedAt = DateTime.UtcNow;
+            Login = string.Empty;
+            PasswordHash = string.Empty;
+        }
+
+        private User(string login, string passwordHash)
+        {
+            Login = login;
+            PasswordHash = passwordHash;
+            JoinedAt = DateTime.UtcNow;
+        }
+
+        public static User Register(string login, string passwordHash)
+        {
+            if (string.IsNullOrEmpty(login))
+                throw new InvariantException("Не указан логин");
+            if (string.IsNullOrEmpty(passwordHash))
+                throw new InvariantException("Не указан пароль");
+
+            return new User(login, passwordHash);
+        }
+    }
+
+    public enum UserType
+    {
+        Guest,
+        User,
+        Admin
     }
 }
