4eac657a86917252537267012fb5ac29d727ba1e
mekk1t <kit.kalimov@gmail.com>
2022-01-25 09:37:33 +0300

    Описал модель шага приготовления рецепта с подробностями ингредиентов.

---
+++ src/KP.Cookbook.Domain/Entities/CookingStep.cs
@@ -0,0 +1,20 @@
+﻿using KP.Cookbook.Domain.ValueObjects;
+
+namespace KP.Cookbook.Domain.Entities
+{
+    public class CookingStep : Entity
+    {
+        public int Order { get; }
+        public string? Description { get; set; }
+        public string? Image { get; set; }
+        public List<IngredientDetailed> Ingredients { get; } = new List<IngredientDetailed>();
+
+        public CookingStep(int order)
+        {
+            if (order <= 0)
+                throw new InvariantException("Не указан порядковый номер шага");
+
+            Order = order;
+        }
+    }
+}
---
+++ src/KP.Cookbook.Domain/ValueObjects/IngredientDetailed.cs
@@ -0,0 +1,50 @@
+﻿using KP.Cookbook.Domain.Entities;
+
+namespace KP.Cookbook.Domain.ValueObjects
+{
+    public class IngredientDetailed : ValueObject
+    {
+        public Ingredient Ingredient { get; }
+        public decimal Amount { get; }
+        public AmountType AmountType { get; }
+        public bool IsOptional { get; set; } = true;
+
+        public IngredientDetailed(Ingredient ingredient, decimal amount, AmountType amountType)
+        {
+            Ingredient = ingredient ?? throw new InvariantException("Ингредиент не может быть пустым.");
+
+            if (AmountMeasurementsAreIncorrect(amount, amountType))
+                throw new InvariantException("Не задано измерение ингредиента.");
+
+            Amount = Math.Round(amount, 1);
+            AmountType = amountType;
+        }
+
+        private static bool AmountMeasurementsAreIncorrect(decimal amount, AmountType amountType)
+        {
+            bool typeIsNotSpecified = amount != 0 && amountType == AmountType.None;
+            bool amountIsNotSpecified = amount == 0 && amountType != AmountType.None;
+
+            return typeIsNotSpecified || amountIsNotSpecified;
+        }
+
+        protected override IEnumerable<object> GetEqualityComponents()
+        {
+            yield return Ingredient;
+            yield return Math.Round(Amount, 1);
+            yield return AmountType;
+        }
+    }
+
+    public enum AmountType
+    {
+        None,
+        GRAM,
+        KILOGRAM,
+        PIECE,
+        MILILITER,
+        LITER,
+        TABLESPOON,
+        TEASPOON
+    }
+}
