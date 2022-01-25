29367cabe4098dbd6c400fc2b9274a5a06eab52c
mekk1t <kit.kalimov@gmail.com>
2022-01-25 09:46:44 +0300

    Описал модель пользователя-автора рецепта

---
+++ src/KP.Cookbook.Domain/Entities/User.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KP.Cookbook.Domain.Entities
+{
+    public class User : Entity
+    {
+        public string Nickname { get; set; }
+        public string Avatar { get; set; }
+        public DateTime JoinedAt { get; set; }
+        public string Login { get; set; }
+        public string PasswordHash { get; set; }
+    }
+}
