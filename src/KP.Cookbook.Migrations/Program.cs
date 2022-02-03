db2cabd480b555dcfe5cb4d7361d9dc954f36cf2
mekk1t <kit.kalimov@gmail.com>
2022-02-03 06:51:08 +0300

    Описал входную точку для модуля миграций

--- src/KP.Cookbook.Migrations/Program.cs
+++ src/KP.Cookbook.Migrations/Program.cs
@@ -1,1 +1,28 @@
-﻿
+﻿using Npgsql;
+using SimpleMigrations;
+using SimpleMigrations.Console;
+using SimpleMigrations.DatabaseProvider;
+
+namespace KP.Cookbook.Migrations
+{
+    class Program
+    {
+        static int Main(string[] args)
+        {
+            if (args.Length == 0)
+                throw new ArgumentException("Не указаны аргументы командной строки");
+
+            var connectionString = args[0];
+            using var connection = new NpgsqlConnection(connectionString);
+
+            var databaseProvider = new PostgresqlDatabaseProvider(connection);
+            var migrator = new SimpleMigrator(typeof(Program).Assembly, databaseProvider);
+
+            migrator.Load();
+
+            var consoleRunner = new ConsoleRunner(migrator);
+
+            return consoleRunner.Run(args);
+        }
+    }
+}
