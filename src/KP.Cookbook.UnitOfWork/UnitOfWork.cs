dafb4aef6a504a1a9bc3f44519926a4f146f8fc1
mekk1t <kit.kalimov@gmail.com>
2022-02-06 08:56:15 +0300

    Набросок реализации UnitOfWork

---
+++ src/KP.Cookbook.UnitOfWork/UnitOfWork.cs
@@ -0,0 +1,33 @@
+﻿using KP.Cookbook.Database;
+using System;
+using System.Collections.Generic;
+using System.Data.Common;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KP.Cookbook.UnitOfWork
+{
+    public class UnitOfWork : IUnitOfWork, IDisposable
+    {
+        private readonly DbConnection _connection;
+        private DbTransaction _transaction;
+
+        public UnitOfWork(Func<DbConnection> connectionFactory)
+        {
+            _connection = connectionFactory();
+            _transaction = _connection.BeginTransaction();
+        }
+
+        public void Dispose()
+        {
+            if (_transaction != null)
+                _transaction.Dispose();
+
+            if (_connection != null)
+                _connection.Dispose();
+        }
+
+        public T Execute<T>(Func<DbConnection, DbTransaction, T> dbQuery) => dbQuery(_connection, _transaction);
+    }
+}
