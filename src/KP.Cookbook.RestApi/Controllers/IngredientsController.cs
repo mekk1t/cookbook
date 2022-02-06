d80f179357eba257a49c7663c30c7b644fd6a387
mekk1t <kit.kalimov@gmail.com>
2022-02-06 09:04:37 +0300

    Подчищаю файлы

---
+++ src/KP.Cookbook.RestApi/Controllers/IngredientsController.cs
@@ -0,0 +1,10 @@
+using Microsoft.AspNetCore.Mvc;
+
+namespace KP.Cookbook.RestApi.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class IngredientsController : ControllerBase
+    {
+    }
+}
