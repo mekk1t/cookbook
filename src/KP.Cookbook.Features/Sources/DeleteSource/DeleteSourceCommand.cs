5480b59a60a61975a4d260e60780284f042ca1ef
mekk1t <kit.kalimov@gmail.com>
2022-02-09 07:45:40 +0300

    Команда: удаление источника

---
+++ src/KP.Cookbook.Features/Sources/DeleteSource/DeleteSourceCommand.cs
@@ -0,0 +1,12 @@
+﻿namespace KP.Cookbook.Features.Sources.DeleteSource
+{
+    public class DeleteSourceCommand
+    {
+        public long SourceId { get; }
+
+        public DeleteSourceCommand(long sourceId)
+        {
+            SourceId = sourceId;
+        }
+    }
+}
---
+++ src/KP.Cookbook.Features/Sources/DeleteSource/DeleteSourceCommandHandler.cs
@@ -0,0 +1,17 @@
+﻿using KP.Cookbook.Cqrs;
+using KP.Cookbook.Database;
+
+namespace KP.Cookbook.Features.Sources.DeleteSource
+{
+    public class DeleteSourceCommandHandler : ICommandHandler<DeleteSourceCommand>
+    {
+        private readonly SourcesRepository _repository;
+
+        public DeleteSourceCommandHandler(SourcesRepository repository)
+        {
+            _repository = repository;
+        }
+
+        public void Execute(DeleteSourceCommand command) => _repository.DeleteById(command.SourceId);
+    }
+}
