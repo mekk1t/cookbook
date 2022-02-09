2d05667b6b7206a82cc5dc01af4bf22a9cc73584
mekk1t <kit.kalimov@gmail.com>
2022-02-09 07:42:39 +0300

    Команда: новый источник

---
+++ src/KP.Cookbook.Features/Sources/CreateSource/CreateSourceCommand.cs
@@ -0,0 +1,14 @@
+﻿using KP.Cookbook.Domain.Entities;
+
+namespace KP.Cookbook.Features.Sources.CreateSource
+{
+    public class CreateSourceCommand
+    {
+        public Source NewSource { get; }
+
+        public CreateSourceCommand(Source newSource)
+        {
+            NewSource = newSource;
+        }
+    }
+}
---
+++ src/KP.Cookbook.Features/Sources/CreateSource/CreateSourceCommandHandler.cs
@@ -0,0 +1,18 @@
+﻿using KP.Cookbook.Cqrs;
+using KP.Cookbook.Database;
+using KP.Cookbook.Domain.Entities;
+
+namespace KP.Cookbook.Features.Sources.CreateSource
+{
+    public class CreateSourceCommandHandler : ICommandHandler<CreateSourceCommand, Source>
+    {
+        private readonly SourcesRepository _repository;
+
+        public CreateSourceCommandHandler(SourcesRepository repository)
+        {
+            _repository = repository;
+        }
+
+        public Source Execute(CreateSourceCommand command) => _repository.Create(command.NewSource);
+    }
+}
