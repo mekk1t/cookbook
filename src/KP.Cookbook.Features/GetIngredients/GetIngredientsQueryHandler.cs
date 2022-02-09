35a9784a64ec370a701efa7e2ebb986004ea9c4e
mekk1t <kit.kalimov@gmail.com>
2022-02-09 07:35:38 +0300

    Запрос: список ингредиентов

---
+++ src/KP.Cookbook.Features/GetIngredients/GetIngredientsQueryHandler.cs
@@ -0,0 +1,18 @@
+﻿using KP.Cookbook.Cqrs;
+using KP.Cookbook.Database;
+using KP.Cookbook.Domain.Entities;
+
+namespace KP.Cookbook.Features.GetIngredients
+{
+    public class GetIngredientsQueryHandler : IQueryHandler<GetIngredientsQuery, List<Ingredient>>
+    {
+        private readonly IngredientsRepository _repository;
+
+        public GetIngredientsQueryHandler(IngredientsRepository repository)
+        {
+            _repository = repository;
+        }
+
+        public List<Ingredient> Execute(GetIngredientsQuery query) => _repository.Get();
+    }
+}
