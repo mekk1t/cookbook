d8f1a5608984f4b9c73837e801ac7d9ea6ab8cfa
mekk1t <kit.kalimov@gmail.com>
2022-02-09 07:44:16 +0300

    Команда: обновление источника

---
+++ src/KP.Cookbook.Features/Sources/UpdateSource/UpdateSourceCommand.cs
@@ -0,0 +1,14 @@
+﻿using KP.Cookbook.Domain.Entities;
+
+namespace KP.Cookbook.Features.Sources.UpdateSource
+{
+    public class UpdateSourceCommand
+    {
+        public Source UpdatedSource { get; }
+
+        public UpdateSourceCommand(Source updatedSource)
+        {
+            UpdatedSource = updatedSource;
+        }
+    }
+}
---
+++ src/KP.Cookbook.Features/Sources/UpdateSource/UpdateSourceCommandHandler.cs
@@ -0,0 +1,17 @@
+﻿using KP.Cookbook.Cqrs;
+using KP.Cookbook.Database;
+
+namespace KP.Cookbook.Features.Sources.UpdateSource
+{
+    public class UpdateSourceCommandHandler : ICommandHandler<UpdateSourceCommand>
+    {
+        private readonly SourcesRepository _repository;
+
+        public UpdateSourceCommandHandler(SourcesRepository repository)
+        {
+            _repository = repository;
+        }
+
+        public void Execute(UpdateSourceCommand command) => _repository.Update(command.UpdatedSource);
+    }
+}
