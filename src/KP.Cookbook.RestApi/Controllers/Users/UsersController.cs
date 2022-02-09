f8012e501314f7a15543afcd039416555e7db1a0
mekk1t <kit.kalimov@gmail.com>
2022-02-09 10:50:54 +0300

    Накинул эндпоинт для регистрации пользователя

---
+++ src/KP.Cookbook.RestApi/Controllers/Users/UsersController.cs
@@ -0,0 +1,35 @@
+﻿using KP.Cookbook.Cqrs;
+using KP.Cookbook.Domain.Entities;
+using KP.Cookbook.Features.Users.CreateUser;
+using KP.Cookbook.Features.Users.GetUsers;
+using KP.Cookbook.RestApi.Controllers.Users.Requests;
+using KP.Cookbook.RestApi.Extensions;
+using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
+using DomainUser = KP.Cookbook.Domain.Entities.User;
+
+namespace KP.Cookbook.RestApi.Controllers.Users
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class UsersController : ControllerBase
+    {
+        private readonly ICommandHandler<CreateUserCommand, DomainUser> _createUser;
+        private readonly IQueryHandler<GetUsersQuery, List<DomainUser>> _getUsers;
+
+        public UsersController(
+            ICommandHandler<CreateUserCommand, DomainUser> createUser,
+            IQueryHandler<GetUsersQuery, List<DomainUser>> getUsers)
+        {
+            _createUser = createUser;
+            _getUsers = getUsers;
+        }
+
+        [HttpGet]
+        public List<DomainUser> GetUsers() => _getUsers.Execute(new GetUsersQuery());
+
+        [HttpPost]
+        public DomainUser Register([FromBody] RegisterUserRequest request) =>
+            _createUser.Execute(new CreateUserCommand(DomainUser.Register(request.Email, request.Password.Sha256Hash())));
+    }
+}
---
+++ src/KP.Cookbook.RestApi/Extensions/StringExtensions.cs
@@ -0,0 +1,20 @@
+﻿using System.Security.Cryptography;
+using System.Text;
+
+namespace KP.Cookbook.RestApi.Extensions
+{
+    public static class StringExtensions
+    {
+        public static string Sha256Hash(this string str)
+        {
+            using var sha256 = SHA256.Create();
+            byte[] bytes = sha256.ComputeHash(Encoding.UTF8.GetBytes(str));
+
+            StringBuilder sb = new();
+            for (int i = 0; i < bytes.Length; i++)
+                sb.Append(bytes[i].ToString("x2"));
+
+            return sb.ToString();
+        }
+    }
+}
