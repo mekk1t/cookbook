77f3f1f742c134fa8b7b51e71d4846e6630f4689
mekk1t <kit.kalimov@gmail.com>
2022-02-09 07:32:23 +0300

    Команда: обновление ингредиента

---
+++ src/KP.Cookbook.Features/UpdateIngredient/UpdateIngredientCommand.cs
@@ -0,0 +1,14 @@
+﻿using KP.Cookbook.Domain.Entities;
+
+namespace KP.Cookbook.Features.UpdateIngredient
+{
+    public class UpdateIngredientCommand
+    {
+        public Ingredient NewIngredient { get; }
+
+        public UpdateIngredientCommand(Ingredient newIngredient)
+        {
+            NewIngredient = newIngredient;
+        }
+    }
+}
---
+++ src/KP.Cookbook.Features/UpdateIngredient/UpdateIngredientCommandHandler.cs
@@ -0,0 +1,17 @@
+﻿using KP.Cookbook.Cqrs;
+using KP.Cookbook.Database;
+
+namespace KP.Cookbook.Features.UpdateIngredient
+{
+    public class UpdateIngredientCommandHandler : ICommandHandler<UpdateIngredientCommand>
+    {
+        private readonly IngredientsRepository _repository;
+
+        public UpdateIngredientCommandHandler(IngredientsRepository repository)
+        {
+            _repository = repository;
+        }
+
+        public void Execute(UpdateIngredientCommand command) => _repository.Update(command.NewIngredient);
+    }
+}
