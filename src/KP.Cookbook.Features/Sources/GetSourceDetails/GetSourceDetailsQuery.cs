d0b7cfec4219e2d3686047b2378bc262165fadf2
mekk1t <kit.kalimov@gmail.com>
2022-02-09 07:40:55 +0300

    Запрос: подробности источника

---
+++ src/KP.Cookbook.Features/Sources/GetSourceDetails/GetSourceDetailsQuery.cs
@@ -0,0 +1,12 @@
+﻿namespace KP.Cookbook.Features.Sources.GetSourceDetails
+{
+    public class GetSourceDetailsQuery
+    {
+        public long SourceId { get; }
+
+        public GetSourceDetailsQuery(long sourceId)
+        {
+            SourceId = sourceId;
+        }
+    }
+}
---
+++ src/KP.Cookbook.Features/Sources/GetSourceDetails/GetSourceDetailsQueryHandler.cs
@@ -0,0 +1,18 @@
+﻿using KP.Cookbook.Cqrs;
+using KP.Cookbook.Database;
+using KP.Cookbook.Domain.Entities;
+
+namespace KP.Cookbook.Features.Sources.GetSourceDetails
+{
+    public class GetSourceDetailsQueryHandler : IQueryHandler<GetSourceDetailsQuery, Source>
+    {
+        private readonly SourcesRepository _repository;
+
+        public GetSourceDetailsQueryHandler(SourcesRepository repository)
+        {
+            _repository = repository;
+        }
+
+        public Source Execute(GetSourceDetailsQuery query) => _repository.GetById(query.SourceId);
+    }
+}
