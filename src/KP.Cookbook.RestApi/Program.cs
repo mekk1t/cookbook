1232decfed5bacfa3fe22836169899bd5059d119
mekk1t <kit.kalimov@gmail.com>
2022-02-09 08:51:44 +0300

    Пофиксил регистрацию зависимостей

--- src/KP.Cookbook.RestApi/Program.cs
+++ src/KP.Cookbook.RestApi/Program.cs
@@ -9,8 +9,12 @@ using Microsoft.AspNetCore.Builder;
 using System;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Hosting;
+using KP.Cookbook.Features.Sources.CreateSource;
+using KP.Cookbook.Features.Sources.UpdateSource;
+using KP.Cookbook.Features.Sources.GetSources;
 
 var container = new Container();
+container.Options.DefaultLifestyle = Lifestyle.Scoped;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -22,14 +26,16 @@ services.AddSwaggerGen();
 
 services.AddSimpleInjector(container, options => options.AddAspNetCore().AddControllerActivation());
 
-var assemblies = AppDomain.CurrentDomain.GetAssemblies();
-
 container.Register<IUnitOfWork, UnitOfWork>();
-container.Register(typeof(Func<DbConnection>), () => new NpgsqlConnection(builder.Configuration.GetConnectionString("Postgresql")));
-container.Register(typeof(ICommandHandler<>), assemblies);
-container.Register(typeof(ICommandHandler<,>), assemblies);
+container.Register<Func<DbConnection>>(() => () => new NpgsqlConnection(builder.Configuration.GetConnectionString("Postgresql")));
+container.Register(typeof(ICommandHandler<>), typeof(UpdateSourceCommandHandler).Assembly);
+container.Register(typeof(ICommandHandler<,>), typeof(CreateSourceCommandHandler).Assembly);
+container.Register(typeof(IQueryHandler<,>), typeof(GetSourcesQuery).Assembly);
 container.RegisterDecorator(typeof(ICommandHandler<>), typeof(UnitOfWorkCommandHandlerDecorator<>));
 container.RegisterDecorator(typeof(ICommandHandler<,>), typeof(UnitOfWorkCommandHandlerDecorator<,>));
+container.Register<UnitOfWork>();
+container.Register<SourcesRepository>();
+container.Register<IngredientsRepository>();
 
 Dapper.DefaultTypeMap.MatchNamesWithUnderscores = true;
 
