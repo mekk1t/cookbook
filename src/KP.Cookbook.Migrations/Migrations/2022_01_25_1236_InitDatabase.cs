ec145c9061008e1e6bafdbac5da6b7c623f16171
mekk1t <kit.kalimov@gmail.com>
2022-04-17 20:18:38 +0300

    Refactoring initial migration

--- src/KP.Cookbook.Migrations/Migrations/2022_01_25_1236_InitDatabase.cs
+++ src/KP.Cookbook.Migrations/Migrations/2022_01_25_1236_InitDatabase.cs
@@ -8,91 +8,91 @@ namespace KP.Cookbook.Migrations.Migrations
         protected override void Up()
         {
             Execute(@"
-				CREATE TABLE ingredients
-				(
-					id 			bigint 		GENERATED ALWAYS AS IDENTITY 	PRIMARY KEY,
-					name 		text		NOT NULL,
-					type 		smallint	NOT NULL,
-					description text		NULL
-				);
+                CREATE TABLE ingredients
+                (
+                    id 			bigint 		GENERATED ALWAYS AS IDENTITY 	PRIMARY KEY,
+                    name 		text		NOT NULL,
+                    type 		smallint	NOT NULL,
+                    description text		NULL
+                );
 
-				CREATE TABLE users
-				(
-					id 				bigint 		GENERATED ALWAYS AS IDENTITY 	PRIMARY KEY,
-					nickname 		text		NULL,
-					avatar			text 		NULL,
-					type			smallint 	NOT NULL,
-					joined_at		timestamptz	NOT NULL,
-					login			text		NOT NULL,
-					password_hash 	text		NOT NULL
-				);
+                CREATE TABLE users
+                (
+                    id 				bigint 		GENERATED ALWAYS AS IDENTITY 	PRIMARY KEY,
+                    nickname 		text		NULL,
+                    avatar			text 		NULL,
+                    type			smallint 	NOT NULL,
+                    joined_at		timestamptz	NOT NULL,
+                    login			text		NOT NULL,
+                    password_hash 	text		NOT NULL
+                );
 
-				CREATE TABLE sources
-				(
-					id 				bigint 		GENERATED ALWAYS AS IDENTITY 	PRIMARY KEY,
-					name 			text		NOT NULL,
-					description		text 		NULL,
-					link			text 		NULL,
-					image			text 		NULL,
-					is_approved		boolean		NOT NULL
-				);
+                CREATE TABLE sources
+                (
+                    id 				bigint 		GENERATED ALWAYS AS IDENTITY 	PRIMARY KEY,
+                    name 			text		NOT NULL,
+                    description		text 		NULL,
+                    link			text 		NULL,
+                    image			text 		NULL,
+                    is_approved		boolean		NOT NULL
+                );
 
-				CREATE TABLE cooking_steps
-				(
-					id 			bigint 		GENERATED ALWAYS AS IDENTITY 	PRIMARY KEY,
-					""order""	smallint	NOT NULL,
-					description	text		NULL,
-					image		text		NULL
-				);
+                CREATE TABLE cooking_steps
+                (
+                    id 			bigint 		GENERATED ALWAYS AS IDENTITY 	PRIMARY KEY,
+                    ""order""	smallint	NOT NULL,
+                    description	text		NULL,
+                    image		text		NULL
+                );
 
                 CREATE TABLE cooking_steps_and_ingredients
                 (
-	                cooking_step_id		bigint			REFERENCES cooking_steps (id) 	ON UPDATE CASCADE ON DELETE CASCADE,
-	                ingredient_id		bigint			REFERENCES ingredients (id) 	ON UPDATE CASCADE ON DELETE CASCADE,
-	                amount				numeric(6, 2) 	NOT NULL,
-	                amount_type			smallint		NOT NULL,
-	                is_optional			boolean			NOT NULL,
+                    cooking_step_id		bigint			REFERENCES cooking_steps (id) 	ON UPDATE CASCADE ON DELETE CASCADE,
+                    ingredient_id		bigint			REFERENCES ingredients (id) 	ON UPDATE CASCADE ON DELETE CASCADE,
+                    amount				numeric(6, 2) 	NOT NULL,
+                    amount_type			smallint		NOT NULL,
+                    is_optional			boolean			NOT NULL,
 
-	                CONSTRAINT cooking_steps_and_ingredients_pk PRIMARY KEY (cooking_step_id, ingredient_id)
+                    CONSTRAINT cooking_steps_and_ingredients_pk PRIMARY KEY (cooking_step_id, ingredient_id)
                 );
 
-				CREATE TABLE recipes
-				(
-					id 				bigint 		GENERATED ALWAYS AS IDENTITY 	PRIMARY KEY,
-					title			text		NOT NULL,
-					recipe_type 	smallint	NOT NULL,
-					cooking_type	smallint 	NOT NULL,
-					kitchen_type	smallint	NOT NULL,
-					holiday_type	smallint	NOT NULL,
-					created_at		timestamptz	NOT NULL,
-					user_id			bigint		REFERENCES users(id) ON DELETE CASCADE			NOT NULL,
-					source_id		bigint		REFERENCES sources(id) ON DELETE SET NULL		NULL,
-					duration_min	smallint	NULL,
-					description		text		NULL,
-					image			text		NULL,
-					updated_at		timestamptz	NULL
-				);
+                CREATE TABLE recipes
+                (
+                    id 				bigint 		GENERATED ALWAYS AS IDENTITY 	PRIMARY KEY,
+                    title			text		NOT NULL,
+                    recipe_type 	smallint	NOT NULL,
+                    cooking_type	smallint 	NOT NULL,
+                    kitchen_type	smallint	NOT NULL,
+                    holiday_type	smallint	NOT NULL,
+                    created_at		timestamptz	NOT NULL,
+                    user_id			bigint		REFERENCES users(id) ON DELETE CASCADE			NOT NULL,
+                    source_id		bigint		REFERENCES sources(id) ON DELETE SET NULL		NULL,
+                    duration_minutes	smallint	NULL,
+                    description		text		NULL,
+                    image			text		NULL,
+                    updated_at		timestamptz	NULL
+                );
 
-				CREATE INDEX recipes_title_index ON recipes USING btree (title);
+                CREATE INDEX recipes_title_index ON recipes USING btree (title);
 
-				CREATE TABLE recipes_and_ingredients
-				(
-					recipe_id			bigint			REFERENCES recipes (id) 	    ON UPDATE CASCADE ON DELETE CASCADE,
-					ingredient_id		bigint			REFERENCES ingredients (id) 	ON UPDATE CASCADE ON DELETE CASCADE,
-					amount				numeric(6, 2) 	NOT NULL,
-					amount_type			smallint		NOT NULL,
-					is_optional			boolean			NOT NULL,
+                CREATE TABLE recipes_and_ingredients
+                (
+                    recipe_id			bigint			REFERENCES recipes (id) 	    ON UPDATE CASCADE ON DELETE CASCADE,
+                    ingredient_id		bigint			REFERENCES ingredients (id) 	ON UPDATE CASCADE ON DELETE CASCADE,
+                    amount				numeric(6, 2) 	NOT NULL,
+                    amount_type			smallint		NOT NULL,
+                    is_optional			boolean			NOT NULL,
 
-					CONSTRAINT recipes_and_ingredients_pk PRIMARY KEY (recipe_id, ingredient_id)
-				);
+                    CONSTRAINT recipes_and_ingredients_pk PRIMARY KEY (recipe_id, ingredient_id)
+                );
 
-				CREATE TABLE recipes_and_cooking_steps
-				(
-					recipe_id			bigint			REFERENCES recipes (id) 		ON UPDATE CASCADE ON DELETE CASCADE,
-					cooking_step_id		bigint			REFERENCES cooking_steps (id) 	ON UPDATE CASCADE ON DELETE CASCADE,
+                CREATE TABLE recipes_and_cooking_steps
+                (
+                    recipe_id			bigint			REFERENCES recipes (id) 		ON UPDATE CASCADE ON DELETE CASCADE,
+                    cooking_step_id		bigint			REFERENCES cooking_steps (id) 	ON UPDATE CASCADE ON DELETE CASCADE,
 
-					CONSTRAINT recipes_and_cooking_steps_pk PRIMARY KEY (recipe_id, cooking_step_id)
-				);
+                    CONSTRAINT recipes_and_cooking_steps_pk PRIMARY KEY (recipe_id, cooking_step_id)
+                );
             ");
         }
 
@@ -104,9 +104,9 @@ namespace KP.Cookbook.Migrations.Migrations
                 DROP TABLE cooking_steps;
                 DROP TABLE cooking_steps_and_ingredients;
 
-				DROP TABLE recipes;
-				DROP TABLE recipes_and_ingredients;
-				DROP TABLE recipes_and_cooking_steps;
+                DROP TABLE recipes;
+                DROP TABLE recipes_and_ingredients;
+                DROP TABLE recipes_and_cooking_steps;
             ");
     }
 }
