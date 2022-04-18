7a8795db88a1fff14ff078a6714ffd2a2c4c09de
mekk1t <kit.kalimov@gmail.com>
2022-04-18 21:35:34 +0300

    DeleteRecipe

---
+++ src/KP.Cookbook.Features/Recipes/DeleteRecipe/DeleteRecipeCommand.cs
@@ -0,0 +1,12 @@
+﻿namespace KP.Cookbook.Features.Recipes.DeleteRecipe
+{
+    public class DeleteRecipeCommand
+    {
+        public long RecipeId { get; }
+
+        public DeleteRecipeCommand(long recipeId)
+        {
+            RecipeId = recipeId;
+        }
+    }
+}
---
+++ src/KP.Cookbook.Features/Recipes/DeleteRecipe/DeleteRecipeCommandHandler.cs
@@ -0,0 +1,17 @@
+﻿using KP.Cookbook.Cqrs;
+using KP.Cookbook.Database;
+
+namespace KP.Cookbook.Features.Recipes.DeleteRecipe
+{
+    public class DeleteRecipeCommandHandler : ICommandHandler<DeleteRecipeCommand>
+    {
+        private readonly RecipesRepository _repository;
+
+        public DeleteRecipeCommandHandler(RecipesRepository repository)
+        {
+            _repository = repository;
+        }
+
+        public void Execute(DeleteRecipeCommand command) => _repository.DeleteById(command.RecipeId);
+    }
+}
