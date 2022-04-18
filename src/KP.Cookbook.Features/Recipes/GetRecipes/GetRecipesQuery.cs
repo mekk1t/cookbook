0012e9ffad258a9831d22a1e3e4aec01f88227fe
mekk1t <kit.kalimov@gmail.com>
2022-04-18 21:51:41 +0300

    GetRecipes

---
+++ src/KP.Cookbook.Features/Recipes/GetRecipes/GetRecipesQuery.cs
@@ -0,0 +1,11 @@
+﻿namespace KP.Cookbook.Features.Recipes.GetRecipes
+{
+    public class GetRecipesQuery
+    {
+        public static GetRecipesQuery Default => new();
+
+        private GetRecipesQuery()
+        {
+        }
+    }
+}
---
+++ src/KP.Cookbook.Features/Recipes/GetRecipes/GetRecipesQueryHandler.cs
@@ -0,0 +1,18 @@
+﻿using KP.Cookbook.Cqrs;
+using KP.Cookbook.Database;
+using KP.Cookbook.Domain.Entities;
+
+namespace KP.Cookbook.Features.Recipes.GetRecipes
+{
+    public class GetRecipesQueryHandler : IQueryHandler<GetRecipesQuery, List<Recipe>>
+    {
+        private readonly RecipesRepository _repository;
+
+        public GetRecipesQueryHandler(RecipesRepository repository)
+        {
+            _repository = repository;
+        }
+
+        public List<Recipe> Execute(GetRecipesQuery query) => _repository.GetRecipes();
+    }
+}
